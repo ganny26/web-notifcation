6a04b4045081739066b016dedf1ad33fb095b393
selvaganeshr <selvaganeshrajam@gmail.com>
2017-05-25 00:31:31 +0530

    mongo configured

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var router = express.Router();
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+const MONGO_URL = "mongodb://localhost:27017/user_email";
+
+var db = mongoose.connect(MONGO_URL);
+
+var userSchema = mongoose.model('user_email',{
+    name:String,
+    email:String
+});
+
+
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
@@ -42,4 +54,34 @@ app.post("/send", function (req, res) {
         socket.emit('getMessage', data);
         res.send(data);
     });
+})
+
+app.get('/userdetails',function(req,res,err){
+    var search = req.query.email;
+    res.send({
+        "payload":search
+    })
+})
+
+app.get('/fetch',function(req,res){
+  
+    userSchema.find({'email':{$regex:/k/}},function(err,result){
+        if(err){
+            console.log(err);
+        }else{
+            res.send({result:result})
+        }
+    })
+})
+app.get('/join',function(req,res,err){
+    var fname = req.query.fname;
+    var lname = req.query.lname
+
+    var name = {
+        "fname":fname,
+        "lname":lname
+    }
+    res.send({
+        "full_name":name
+    })
 })
